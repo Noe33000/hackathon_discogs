91b7fc2327b95d56082cdfe897e362677f610d6d
noe33000 <champignyn@gmail.com>
2017-05-04 15:15:53 +0200

    Loop to get many artists and display them on home page

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -8,6 +8,24 @@ angular.module("discogs")
 
 
 function Home(Artist) {
-    this.artist = Artist.get({id: "3422"});
+    let artists = [
+        "791",      // Mr Oizo
+        "65049",   // Notorious
+        //"133768",   // Big Pun
+        //"41441",    // Bob Marley
+        //"10584",    // Nina Simone
+        //"15588",    // Gonzalez
+        //"3122966"   // Fauve
+    ];
+    let artistsLength = artists.length;
+    
+    this.artist = [];
+    
+    for(let i = 0; i < artistsLength; i++){
+        this.artist.push(Artist.get({id: artists[i]}));
+    }
+
     this.test = "testIsOk";
+
+
 }
